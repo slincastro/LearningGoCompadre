ef8ae0e9a5b426f72419b962399a0fea07051c53
Slin castro <scastro@thoughtworks.com>
2019-09-03 12:52:22 -0500

    include json response on web service

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 
 ##Console App 
 Console Application in Golang, Read and filter Logs
+
+run:
+
+`go run .`
+
+##Web Service
+Basic Web Service :
+
+run:
+
+`go run .`
+
+Test
+
+`curl http://localhost:3000/\?name\=AnyName`
--- WebService/main.go
+++ WebService/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
+		names := r.URL.Query()["name"]
+		var name string
+
+		if len(names) == 1 {
+			name = names[0]
+		}
+
+		m := map[string]string{"name": name}
+		enc := json.NewEncoder(w)
+		enc.Encode(m)
 	})
 
 	err := http.ListenAndServe(":3000", nil)
